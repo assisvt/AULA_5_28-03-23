45f1142169bde7076e2412c11ee205ac3c07e397
Vítor Assis <contatoassisvt@gmail.com>
2023-03-28 10:44:58 -0300

    Create while_pt2.py

---
+++ while_pt2.py
@@ -0,0 +1,18 @@
+ct = 0
+soma = 0
+# ou ct = soma = 0
+print('Digite [0] para sair')
+while True:           # while valor != 0:
+    valor = int(input('Digite um número: '))
+    if valor == 0:    # Valor igual (==) a 0 é a condição de saída
+        break         # O break força a saída da estrutura de repetição (while)
+    resto = valor % 2 # O operador % pega o resto da divisão
+    if resto == 0:    # Se o resto é zero o valor é par
+        soma = soma + valor # soma += valor #incrementa a soma de valores
+        ct = ct + 1    # ct+= 1 # incrementa a contagem de notas
+    # Fim da estrutura de repetição "while"
+if ct != 0:
+    media = soma/ct
+    print("A média de todos os pares é:", media)
+else:
+    print('Não foi digitado um número par.')

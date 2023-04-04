e9f08898b3255b71533285afdcdea2b71574c57a
Vítor Assis <contatoassisvt@gmail.com>
2023-04-04 10:49:41 -0300

    Update Aula_5_while.py

--- Aula_5_while.py
+++ Aula_5_while.py
@@ -46,3 +46,27 @@ print(f'A média da turma de {ct} alunos é {media:.2f}')
 # ou, colocar isso em vez do print acima.
 # print('A soma das notas do alunos é:', soma, end = " ")
 # print(f"e a média da turma é = {media:.2f}")
+--
+(editar e arrumar erro)
+ct_masc = 0
+ct_fem = 0
+variavel_maior = -99999999
+variavel_menor = 999999
+print('Digite [0] para sair')
+while True:
+    altura = float(input('Digite a altura: '))
+    if altura == 0: # testa condição saída
+        break
+    genero = input('Digite [m] para Masculino ou [f] para Feminino: ') # Lê gênero, quando for digitar uma letra, ele já assume que é str, mas posso por str ou deixar em branco.
+    if genero == 'm': # Pra comparar eu uso as aspas, por isso m está entre aspas. lê: se gênero é masculino
+        ct_masc = ct_masc + 1 # ct_mas += 1
+    else:
+        ct_fem = ct_fem + 1
+    if altura > maior:
+        maior = altura # atualiza a maior altura
+    if altura < menor:  # não pode usar else
+        menor = altura  # Fim da estrutura de rep. while
+print('Maior altura no grupo:', maior) # print(f'Maior altura no grupo: {maior:.2f}')
+print('Menor altura no grupo:', menor) # print('Menor altura: {:.2f}' .format(menor))
+print('Quantidade de homens:', ct_masc)
+print('Quantidade de mulheres:', ct_fem)
